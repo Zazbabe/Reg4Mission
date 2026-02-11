a6bbfb1dcedc9bd01847d1c39aa5cb41a2c064e7
Zazbabe <zazelfi@hotmail.com>
2026-02-11 11:10:34 +0100

    first commit

---
+++ Reg4MissionX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reg4MissionX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
