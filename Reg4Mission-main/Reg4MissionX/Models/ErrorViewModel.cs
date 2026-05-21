b9795f064187c16e5941809ca0bd8fda271e6f46
PrincessLemon <emeliesvensson1337@gmail.com>
2026-05-21 10:06:28 +0200

    Delete Reg4Mission-main directory

--- Reg4Mission-main/Reg4MissionX/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Reg4MissionX.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
